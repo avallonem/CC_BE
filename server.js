79d395ecdcd8fdc47014d7d24f59b0eb278da438
AVALLONE MARCO (CA) <AVALLO36@posteitaliane.it>
2020-12-09 17:23:45 +0100

    eigth commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://cc-pilots-vm2.westeurope.cloudapp.azure.com:3000"
 };
 
 app.use(cors(corsOptions));
